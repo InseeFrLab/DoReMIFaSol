03d8b63b32c99bf2eab43b90e04eefc0465edf4c
pierre-lamarche <lamarche.p@gmail.com>
2020-04-26 18:00:23 +0200

    Correction encoding UTF-8 sur la table bpe_ens_xy_2018

--- data-raw/bpe_ens_xy_2018.R
+++ data-raw/bpe_ens_xy_2018.R
@@ -1,7 +1,8 @@
 #' Base de données Base Permanentes des Équipements Géoloaclisés
 #' La base permanente des équipements (BPE) est une source statistique qui fournit le niveau d'équipements et de services rendus à la population sur un territoire. Les résultats sont proposés sous forme de bases de données dans différents formats et pour deux niveaux géographiques : les communes et les Iris.
 #'
+#' @encoding UTF-8
 #' @source \url{https://www.insee.fr/fr/statistiques/fichier/3568638/bpe18_ensemble_xy_csv.zip}
-
-bpe_ens_xy_2018 <- telechargerDonnees(donnees = "BPE_ENS_XY", date = as.Date("01/01/2018"))
+bpe_ens_xy_2018 <- telechargerDonnees(donnees = "BPE_ENS_XY", date = as.Date("01/01/2018"), encoding = "CP1252")
+Encoding(levels(bpe_ens_xy_2018$QUALITE_XY)) <- "UTF-8" ## Solves encoding issue
 usethis::use_data(bpe_ens_xy_2018, overwrite = TRUE)
