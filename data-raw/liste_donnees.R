f556eff18413dc990c02d813dbe0f7958ff835e4
pierre-lamarche <lamarche.p@gmail.com>
2020-04-26 13:50:37 +0200

    ajout usethis::use_data

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -1,4 +1,3 @@
 liste_donnees <- read.csv("data/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "Date", "character",
                                                                                              "character", "logical", "character", "character"))
-
-save(liste_donnees, file = "data/liste_donnees.rda")
+usethis::use_data(liste_donnees)
