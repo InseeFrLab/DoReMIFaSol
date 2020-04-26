6749b8ecf25cbcc3cb39aafc2b80624f8392ac32
pierre-lamarche <lamarche.p@gmail.com>
2020-04-27 00:11:49 +0200

    Création table système

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -2,3 +2,7 @@ liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ","
                                                                                              "character", "logical", "character", "character"),
                           encoding = "UTF-8", fileEncoding = "UTF-8")
 usethis::use_data(liste_donnees, overwrite = TRUE)
+
+## internal
+ld <- liste_donnees
+usethis::use_data(ld, internal = TRUE, overwrite = TRUE)
