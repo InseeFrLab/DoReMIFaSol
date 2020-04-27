19b09c054fed15fb093286c1b892ff9f5cee8edb
pierre-lamarche <lamarche.p@gmail.com>
2020-04-27 21:23:35 +0200

    Données

--- data-raw/filosofi.R
+++ data-raw/filosofi.R
@@ -1,19 +1,19 @@
 ## Filosofi
 ##2015
 # niveau communal
-filosofi_com_2015 <- telechargerDonnees(donnees = "FILOSOFI_COM_2015", date = as.Date("01/01/2015"))
+filosofi_com_2015 <- telechargerDonnees(donnees = "FILOSOFI_COM", date = as.Date("01/01/2015", format = "%d/%m/%Y"))
 Encoding(filosofi_com_2015$LIBGEO) <- "UTF-8" ## Solves encoding issue
 usethis::use_data(filosofi_com_2015, overwrite = TRUE)
 # niveau EPCI
-filosofi_epci_2015 <- telechargerDonnees(donnees = "FILOSOFI_EPCI_2015", date = as.Date("01/01/2015"))
+filosofi_epci_2015 <- telechargerDonnees(donnees = "FILOSOFI_EPCI", date = as.Date("01/01/2015", format = "%d/%m/%Y"))
 Encoding(filosofi_epci_2015$LIBGEO) <- "UTF-8" ## Solves encoding issue
 usethis::use_data(filosofi_epci_2015, overwrite = TRUE)
 ##2016
 # niveau communal
-filosofi_com_2016 <- telechargerDonnees(donnees = "FILOSOFI_COM_2016", date = as.Date("01/01/2016"))
+filosofi_com_2016 <- telechargerDonnees(donnees = "FILOSOFI_COM", date = as.Date("01/01/2016", format = "%d/%m/%Y"))
 Encoding(filosofi_com_2016$LIBGEO) <- "UTF-8" ## Solves encoding issue
 usethis::use_data(filosofi_com_2016, overwrite = TRUE)
 # niveau EPCI
-filosofi_epci_2016 <- telechargerDonnees(donnees = "FILOSOFI_EPCI_2016", date = as.Date("01/01/2016"))
+filosofi_epci_2016 <- telechargerDonnees(donnees = "FILOSOFI_EPCI", date = as.Date("01/01/2016", format = "%d/%m/%Y"))
 Encoding(filosofi_epci_2016$LIBGEO) <- "UTF-8" ## Solves encoding issue
 usethis::use_data(filosofi_epci_2016, overwrite = TRUE)
