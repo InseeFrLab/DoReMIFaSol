7b53ad8707259f301c25f4af1a55a7a40e44b827
Olivier Meslin <Olivier@macbook-de-olivier-meslin.local>
2020-04-27 09:24:08 +0200

    Ajout du COG 2019

---
+++ data-raw/cog_communes_2019.R
@@ -0,0 +1,10 @@
+#' Code Officiel Géographique
+#' Le Code Officiel Géographique est une source statistique de référence publiée par l’Insee, 
+#' qui rassemble les codifications (numérotations et libellés) officielles des communes, 
+#' des cantons, des arrondissements, des départements, des régions, des collectivités d'outre-mer, des pays et territoires étrangers.
+#'
+#'
+#' @source \url{https://www.insee.fr/fr/statistiques/fichier/3720946/cog_ensemble_2019_csv.zip}
+
+cog_communes_2019 <- telechargerDonnees(donnees = "cog_communes", date = as.Date("01/01/2019"))
+usethis::use_data(cog_communes_2019, overwrite = TRUE)
