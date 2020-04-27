40a9840b176e54163172b87b274afe675d98e2bd
Olivier Meslin <Olivier@macbook-de-olivier-meslin.local>
2020-04-27 15:44:51 +0200

    En l'état

--- R/telechargerDonnees.R
+++ R/telechargerDonnees.R
@@ -23,34 +23,47 @@ telechargerDonnees <- function(donnees, date=NULL, telDir=NULL, ...) {
     if (is.null(date)) stop("Il faut sp\u00e9cifier une date de r\u00e9f\u00e9rence pour ces donn\u00e9es.")
     caract <- caract[caract$date_ref == date]
   }
-  
+
   #dossier de téléchargement
   if (is.null(telDir)) {
     if (is.null(getOption("insee.cache"))) {
-      telDir <- tempdir()
+      telDir <- gsub("//", "/", tempdir())
       options(insee.cache = telDir)
+      print(telDir)
     } else {
-      telDir <- getOption("insee.cache")
+      telDir <- gsub("//", "/", getOption("insee.cache"))
     }
   }
-  
-  nomFichier <- paste0(telDir, "/", tail(unlist(strsplit(caract$lien, "/")), n=1L))
+  nomFichierTemp <- tail(unlist(strsplit(caract$lien, "/")), n=1L)
+  print(nomFichierTemp)
+  nomFichier <- paste0(telDir, "/", nomFichierTemp)
+  print(nomFichier)
   #stringr::str_extract(url, "^*([^/]*)$")
   if (!file.exists(nomFichier))
     download.file(url = caract$lien, destfile = nomFichier) else
       message("utilisation du cache")
   
   if (caract$zip) {
-    if (substr(nomFichier, nchar(nomFichier) - 3, nchar(nomFichier)) != ".zip") 
+    print("zip")
+    if (substr(nomFichier, nchar(nomFichier) - 3, nchar(nomFichier)) != ".zip") {
       stop("Le fichier t\u00e9l\u00e9charg\u00e9 n'est pas une archive zip.")
+    } else {
+      print("rezip0")
+      unzip(nomFichier, files = caract$fichier_donnees, exdir = )
+      fichierAImporter <- paste0(substr(nomFichier, 1, nchar(nomFichier) - 4), "/", caract$fichier_donnees)
+      print(fichierAImporter)
+      print("rezip")    
+    }
   } else {
-    if (substr(nomFichier, nchar(nomFichier) - 4, nchar(nomFichier)) != paste0(".", caract$type))
+    if (substr(nomFichier, nchar(nomFichier) - 4, nchar(nomFichier)) != paste0(".", caract$type)) 
       stop("le fichier t\u00e9l\u00e9charg\u00e9 n'est pas du type attendu.")
     fichierAImporter <- nomFichier
   }
+  print(fichierAImporter)
+  
   # importation donnees
   if (caract$type == "csv")
-    res <- read.csv(fichierAImporter, sep = caract$separateur, header = TRUE, ...)
+    res <- readr::read_delim(fichierAImporter, delim = caract$separateur, col_names = TRUE, ...)
   if (caract$type == "xls")
     res <- readxl::read_xls(fichierAImporter, sheet = caract$onglet, skip = caract$premiere_ligne - 1)
   file.remove(fichierAImporter)
