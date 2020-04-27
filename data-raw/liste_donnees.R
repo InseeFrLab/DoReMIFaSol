fdb0d11eae747d681b715aa35dfaaf82243d34a5
Olivier Meslin <Olivier@macbook-de-olivier-meslin.local>
2020-04-27 09:47:32 +0200

    Modification du séparateur pour l'importation du csv

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -1,4 +1,4 @@
-liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "Date", "character",
+liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ";", colClasses = c("character", "character", "Date", "character",
                                                                                              "character", "logical", "character", "character"),
                           encoding = "UTF-8", fileEncoding = "UTF-8")
 usethis::use_data(liste_donnees, overwrite = TRUE)
