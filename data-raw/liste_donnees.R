abe0242217833dd27f534591584a1d81cb4cc9db
pierre-lamarche <lamarche.p@gmail.com>
2020-04-27 21:22:32 +0200

    Re-générer liste_donnees

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -1,9 +1,10 @@
-liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "Date", "character",
+liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "character", "character",
                                                                                              "character", "logical", "character", "character",
                                                                                              "character", "integer", "character"),
                           encoding = "UTF-8", fileEncoding = "UTF-8", na.strings = c("", " "))
-liste_donnees <- within(liste_donnees,
-                        separateur <- gsub("\"", "", separateur))
+liste_donnees <- within(liste_donnees, {
+                        separateur <- gsub("\"", "", separateur)
+                        date_ref = as.Date(date_ref, format = "%d/%m/%Y")})
 usethis::use_data(liste_donnees, overwrite = TRUE)
 
 ## internal
