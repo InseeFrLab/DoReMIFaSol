4cfd14e718f2cbc530e090cd59f2424d594edaac
pierre-lamarche <lamarche.p@gmail.com>
2020-04-27 09:03:51 +0200

    Tests unitaires

---
+++ tests/testthat/test_telechargerDonnees.R
@@ -0,0 +1,13 @@
+library(carpenteR)
+## téléchargement de la BPE
+test_that("Téléchargement de données sur le site de l'Insee", {
+  expect_output(class(telechargerDonnees("BPE_ENS", date = as.Date("01/01/2018"))), "data.frame")
+})
+## erreur - oubli de la date
+test_that("Téléchargement de données sur le site de l'Insee", {
+  expect_error(telechargerDonnees("BPE_ENS"))
+})
+## mauvais nom - pas disponible au téléchargement
+test_that("Échec du téléchargement pour nom non existant", {
+  expect_error((telechargerDonnees("TEST")))
+})
