bdb8af9f0f79bda9dc245c3842c5d6049d326aab
pierre-lamarche <lamarche.p@gmail.com>
2020-05-12 22:02:07 +0200

    Correction typo

--- tests/testthat/test_telechargerDonnees.R
+++ tests/testthat/test_telechargerDonnees.R
@@ -28,7 +28,7 @@ test_that("Importation type CSV - output data.frame", {
 })
 ## test import de données XLS
 test_that("Importation type XLS - output data.frame", {
-  expect_true(class(telechargerDonnees("FILOSIFI_COM", date = as.Date("01/01/2014", format = "%d/%m/%Y"))) == "data.frame")
+  expect_true(class(telechargerDonnees("FILOSOFI_COM", date = as.Date("01/01/2014", format = "%d/%m/%Y"))) == "data.frame")
 })
 ## test import de données XLSX
 test_that("Importation type XLSX - output data.frame", {
