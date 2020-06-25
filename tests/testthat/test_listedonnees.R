89046a46cdde771d31a2c3e30690412c5d1c0c7d
pierre-lamarche <lamarche.p@gmail.com>
2020-06-25 15:29:01 +0200

    Ajout tests sur la validité du fichier de données

--- tests/testthat/test_listedonnees.R
+++ tests/testthat/test_listedonnees.R
@@ -3,3 +3,14 @@ library(doremifasol)
 test_that("Teste l'ensemble des URL de la liste", {
   expect_true(all(sapply(unique(ld$lien), url.exists)))
 })
+## test unicité du couple nom - date_ref
+test_that("Teste l'unicité du couple nom - millésime", {
+  expect_true(all(!duplicated(ld[, c("nom", "date_ref")])))
+})
+## test bonne spécification du fichier Excel
+test_that("Teste l'existence de l'onglet", {
+  expect_true(all(with(ld, type %in% c("xls", "xlsx") & !is.na(onglet) | !type %in% c("xls", "xlsx"))))
+})
+test_that("Teste l'existence de l'onglet", {
+  expect_true(all(with(ld, type %in% c("xls", "xlsx") & !is.na(premiere_ligne) | !type %in% c("xls", "xlsx"))))
+})
