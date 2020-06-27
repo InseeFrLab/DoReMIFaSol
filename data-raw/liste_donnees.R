968db1d1c32b7d5b9f633f1a41465c01753f56a8
pierre-lamarche <lamarche.p@gmail.com>
2020-06-27 15:04:38 +0200

    Ajout colonne valeur_manquante

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -1,6 +1,7 @@
 liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "character", "character",
                                                                                                  "character", "logical", "character", "character",
-                                                                                                 "character", "integer", "integer", "character"),
+                                                                                                 "character", "integer", "integer", "character", 
+                                                                                                 "character"),
                           encoding = "UTF-8", fileEncoding = "UTF-8", na.strings = c("", " "))
 liste_donnees <- within(liste_donnees, {
   #separateur <- gsub("\"", "", separateur)
