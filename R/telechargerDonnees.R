5b6580b51b69755a3b0f46bce06dcaed369f4a3f
pierre-lamarche <lamarche.p@gmail.com>
2020-06-28 23:08:42 +0200

    Change la dépendance de stringr vers stringi

--- R/telechargerDonnees.R
+++ R/telechargerDonnees.R
@@ -47,7 +47,7 @@ telechargerDonnees <- function(donnees, date=NULL, telDir=NULL, ...) {
       fichierAImporter <- paste0(telDir, "/", caract$fichier_donnees)
     }
   } else {
-    if (stringr::str_extract(nomFichier, "^*([^.]*)$") != caract$type) 
+    if (stringi::stri_match_last_regex(fic, "^*.(\\w*)$")[, 2] != caract$type) 
       stop("le fichier t\u00e9l\u00e9charg\u00e9 n'est pas du type attendu.")
     fichierAImporter <- nomFichier
   }
