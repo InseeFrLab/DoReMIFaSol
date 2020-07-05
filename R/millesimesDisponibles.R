9ef56a81cc300d29ca826defe387f2648f3e824b
Pierre Lamarche <lamarche.p@gmail.com>
2020-07-05 17:53:32 +0000

    Ajout fonction millesimesDisponibles

---
+++ R/millesimesDisponibles.R
@@ -0,0 +1,18 @@
+#' Lister les différents millésimes pour une source donnée
+#'
+#' @param donnees une chaîne de caractères qui identifie la source de données
+#'
+#' @return un vecteur contenant l'ensemble des millésimes disponibles pour une source de données.
+#' @export
+#'
+#' @examples
+#' millesimesDisponibles("RP_LOGEMENT")
+millesimesDisponibles <- function(donnees) {
+  ## check the parameter donnees takes a valid value
+  if (!donnees %in% ld$nom)
+    stop("Le param\u00e8tre donnees est mal sp\u00e9cifi\u00e9, la valeur n'est pas r\u00e9f\u00e9renc\u00e9e")
+  liste <- with(ld, date_ref[nom == donnees])
+  if (!any(duplicated(format(liste, "%Y"))))
+    return(format(liste, "%Y")) else 
+      return(liste)
+}
