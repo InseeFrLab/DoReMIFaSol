d0aaf4b481441f5af7b5c479233f3a501bd57600
Pierre-Yves Berrard <pierre.yves.berrard@gmail.com>
2020-10-07 13:40:21 +0200

    Ajoute `consulter`

---
+++ R/consulter.R
@@ -0,0 +1,26 @@
+#' Consulter le descriptif des données sur insee.fr
+#'
+#' @inheritParams telechargerFichier
+#' @param donnees le nom des données dont on veut consulter la page sur le site de l'Insee. La description complète des données associées à ce nom figure dans la table ([liste_donnees]).
+#'
+#' @return La fonction ouvre le lien dans le navigateur. Elle retourne accessoirement le lien vers la page web, de manière invisible.
+#'
+#' @export
+#'
+#' @examples
+#' consulter(donnees = "BPE_ENS")
+#' consulter("RP_LOGEMENT", date = "2016")
+
+consulter <- function(donnees, date = NULL) {
+
+  caract <- infosDonnees(donnees, date)
+
+  # construit l'url web à partir de l'url du fichier
+  url <- sub("fichier/([0-9]+)/.+$", "\\1", caract$lien)
+  
+  # ouvre dans navigateur
+  utils::browseURL(url)
+  
+  invisible(url)
+
+}
---
+++ tests/testthat/test_consulter.R
@@ -0,0 +1,17 @@
+library(doremifasol)
+
+context("consulter")
+
+test_that("url web correctes", {
+  
+  expect_equal(
+    consulter(donnees = "BPE_ENS"),
+    "https://www.insee.fr/fr/statistiques/3568629"
+  )
+  
+  expect_equal(
+    consulter("RP_LOGEMENT", date = "2016"),
+    "https://www.insee.fr/fr/statistiques/4229099"
+  )  
+  
+})
