4392110a14c2e96d552e869ce03eefcea470ccd0
pierre-lamarche <lamarche.p@gmail.com>
2020-10-28 13:29:51 +0100

    Modification nature de ld (json)

--- data-raw/liste_donnees.R
+++ data-raw/liste_donnees.R
@@ -1,13 +1,15 @@
 liste_donnees <- read.csv("data-raw/liste_donnees.csv", header = TRUE, sep = ",", colClasses = c("character", "character", "character", "character", "character",
                                                                                                  "character", "logical", "logical", "character", "character",
                                                                                                  "character", "integer", "integer", "character", 
-                                                                                                 "character", "character", "logical"),
+                                                                                                 "character", "character", "logical", "character", "integer"),
                           encoding = "UTF-8", fileEncoding = "UTF-8", na.strings = c("", " "))
 liste_donnees <- within(liste_donnees, {
   #separateur <- gsub("\"", "", separateur)
   separateur <- ifelse(is.na(separateur), NA, paste0("quote(", separateur, ")"))
-  date_ref = as.Date(date_ref, format = "%d/%m/%Y")})
+  date_ref <- as.Date(date_ref, format = "%d/%m/%Y")})
 usethis::use_data(liste_donnees, overwrite = TRUE)
+
+liste_var_ld <- names(liste_donnees)
 
 ## internal
 #ld <- liste_donnees
@@ -18,4 +20,10 @@ write(jsonlite::toJSON(liste_donnees, pretty = TRUE), file = "data-raw/liste_don
 
 ## internal
 ld <- jsonlite::read_json("data-raw/liste_donnees.json")
-usethis::use_data(ld, internal = TRUE, overwrite = TRUE)
+ld <- lapply(ld, function(x) {
+  within(x, {
+    if (!is.null(x$date_ref))
+      date_ref <- as.Date(date_ref, format = "%Y-%m-%d")
+  })
+})
+usethis::use_data(ld, liste_var_ld, internal = TRUE, overwrite = TRUE)
