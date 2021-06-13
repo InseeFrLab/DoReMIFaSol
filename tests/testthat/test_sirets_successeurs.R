180481c91fdd1ad567567ea4f7162fc0730ef32c
Pierre <lamarche.p@gmail.com>
2021-06-13 09:11:11 +0200

    Réécriture test sirets_successeurs

--- tests/testthat/test_sirets_successeurs.R
+++ tests/testthat/test_sirets_successeurs.R
@@ -2,5 +2,6 @@
 test_that("Téléchargement des successeurs", {
   successeurs <- sirets_successeurs(c("30070230500040", "30137492200120", "30082187300019"))
   print(successeurs)
-  expect_true(class(successeurs)[1] == "insee_data_frame")
+  expect_s3_class(successeurs, 
+  c("insee_data_frame", "data.frame"))
 })
