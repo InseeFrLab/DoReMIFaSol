98599f376a472a06389642b5afe27b43933cb376
pierre-lamarche <lamarche.p@gmail.com>
2022-01-30 15:09:22 +0100

    ajout message de démarrage

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,12 @@
   
   res <- lapply(liste_url, httr::GET)
   if (any(sapply(res, function(x) x$status_code != 200))){
+    idx <- which(sapply(res, function(x) x$status_code == 200))[1]
     writeBin(res[[which(sapply(res, function(x) x$status_code == 200))[1]]]$content, "liste_donnees.json")
-    ld <- jsonlite::fromJSON("liste_donnees.json")
+    liste_donnees <- jsonlite::fromJSON("liste_donnees.json")
+    cat("Au d\u00e9marrage, chargement de la liste de donn\u00e9es depuis :\n", liste_url[2])
+  } else {
+    cat("Pas de mise \u00e0 jour disponible, liste de donn\u00e9es issue du package.")
   }
   
 }
