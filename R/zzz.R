9646fbf91d9a56936eedcff8fd285b514e23d43f
pierre-lamarche <lamarche.p@gmail.com>
2022-01-30 15:50:04 +0100

    changer ld et liste_donnees

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@
   if (any(sapply(res, function(x) x$status_code != 200))){
     idx <- which(sapply(res, function(x) x$status_code == 200))[1]
     writeBin(res[[which(sapply(res, function(x) x$status_code == 200))[1]]]$content, "liste_donnees.json")
-    liste_donnees <- jsonlite::fromJSON("liste_donnees.json")
+    liste_donnees <- ld <- jsonlite::fromJSON("liste_donnees.json")
     cat("Au d\u00e9marrage, chargement de la liste de donn\u00e9es depuis :\n", liste_url[2])
   } else {
     cat("Pas de mise \u00e0 jour disponible, liste de donn\u00e9es issue du package.")
