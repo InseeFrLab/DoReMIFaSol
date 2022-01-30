4e6ecda092d08b44224165a85da38789dcc95295
pierre-lamarche <lamarche.p@gmail.com>
2022-01-30 14:32:44 +0100

    at startup, load ld updated

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(libname, pkgname) {
   #data(liste_donnees)
+  liste_url <- c("https://minio.lab.sspcloud.fr/donnees-insee/liste_donnees.json",
+                 "https://raw.githubusercontent.com/InseeFrLab/DoReMIFaSol/master/data-raw/liste_donnees.json")
+  
+  res <- lapply(liste_url, httr::GET)
+  if (any(sapply(res, function(x) x$status_code != 200))){
+    writeBin(res[[which(sapply(res, function(x) x$status_code == 200))[1]]]$content, "liste_donnees.json")
+    ld <- jsonlite::fromJSON("liste_donnees.json")
+  }
+  
 }
