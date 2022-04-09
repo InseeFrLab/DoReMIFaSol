2165544170c94c5e00f831a79fe5cdb430616532
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2022-04-10 00:19:55 +0200

    améliore consulter (url_only)

--- R/consulter.R
+++ R/consulter.R
@@ -2,33 +2,33 @@
 #'
 #' @inheritParams telechargerFichier
 #' @param donnees le nom des données dont on veut consulter la page sur le site de l'Insee. La description complète des données associées à ce nom figure dans la table ([liste_donnees]).
-#' @param consultation Booléen qui indique si on veut consulter directement consulter la page sur son navigateur (TRUE) ou seulement récupérer l'URL de la page (FALSE).
+#' @param url_only `TRUE` pour seulement récupérer l'URL de la page sans ouvrir le navigateur.
 #'
-#' @return La fonction ouvre le lien dans le navigateur. Elle retourne accessoirement le lien vers la page web, de manière invisible.
+#' @return Par défaut, la fonction ouvre le lien dans le navigateur. Elle retourne accessoirement le lien vers la page web, de manière invisible.
 #'
 #' @export
 #'
 #' @examples
 #' consulter(donnees = "BPE_ENS")
 #' consulter("RP_LOGEMENT", date = "2016")
-#' # Pour récupérer seulement l'URL de la page
-#' consulter("RP_LOGEMENT", date = "2016", consultation = FALSE)
+#' # Pour seulement obtenir l'URL de la page
+#' consulter("RP_LOGEMENT", date = "2016", url_only = TRUE)
 
-consulter <- function(donnees, date = NULL, consultation = TRUE) {
+consulter <- function(donnees, date = NULL, url_only = FALSE) {
 
-  caract <- infosDonnees(donnees, date, consultation)
+  caract <- infosDonnees(donnees, date, silencieux = url_only)
 
   # construit l'url web à partir de l'url du fichier
   url <- sub("fichier/([0-9]+)/.+$", "\\1", caract$lien)
   if (caract$collection == "GEOGRAPHIE") {
     url <- sub("/statistiques/", "/information/", url)
   }
-  
-  if (isTRUE(consultation)) {
-    # ouvre dans navigateur
-    utils::browseURL(url)
-  } 
-  
+
+  if (url_only) return(url)
+
+  # ouvre dans navigateur
+  utils::browseURL(url)
+
   invisible(url)
 
 }
