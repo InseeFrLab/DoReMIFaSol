22afc9a4e1f41df81a493391196896ee66812bbf
Pierre Lamarche <lamarche.p@gmail.com>
2025-01-01 15:14:24 +0100

    prise en compte champ url_documentation

--- R/consulter.R
+++ R/consulter.R
@@ -18,12 +18,16 @@ consulter <- function(donnees, date = NULL, url_only = FALSE) {
 
   caract <- infosDonnees(donnees, date, silencieux = url_only)
 
-  # construit l'url web à partir de l'url du fichier
-  url <- sub("fichier/([0-9]+)/.+$", "\\1", caract$lien)
-  
-  # des exceptions sur les url web pour la collection GEOGRAPHIE et le fichier des personnes décédées
-  if (caract$collection == "GEOGRAPHIE" || substr(caract$nom, 1, 8) == "DECES_20") {
-    url <- sub("/statistiques/", "/information/", url)
+  if (is.null(caract$url_documentation)) {
+    # construit l'url web à partir de l'url du fichier
+    url <- sub("fichier/([0-9]+)/.+$", "\\1", caract$lien)
+    
+    # des exceptions sur les url web pour la collection GEOGRAPHIE et le fichier des personnes décédées
+    if (caract$collection == "GEOGRAPHIE" || substr(caract$nom, 1, 8) == "DECES_20") {
+      url <- sub("/statistiques/", "/information/", url)
+    }
+  } else {
+    url <- caract$url_documentation
   }
 
   if (url_only) return(url)
