28f2f364f2da5927415477190ff166957af64e2a
Pierre Lamarche <lamarche.p@gmail.com>
2025-01-01 23:36:11 +0100

    charge la liste de donnees complete

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   #data(liste_donnees)
+  if (curl::has_internet())
+    ld_melodi <- httr::GET("https://minio.lab.sspcloud.fr/pierrelamarche/melodi/liste_donnees.json")
+    assign("ld", c(ld, jsonlite::fromJSON(httr::content(ld_melodi, as = "text", encoding = "utf-8"), 
+                                          simplifyDataFrame = FALSE)),
+           envir = asNamespace("doremifasol")
+          )
 }
